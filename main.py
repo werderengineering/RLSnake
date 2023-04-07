7b6dd016741ca9a818f0eb9e91d24bd50e8b13b2
Werder <0nel4s7h0n0r@gmail.com>
2023-04-06 20:31:54 -0400

    Launch

---
+++ main.py
@@ -0,0 +1,21 @@
+#This is the main file to run RLsnake
+import numpy as np
+def test(Num1,Num2):
+    sum=Num1+Num2
+    return sum
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print('Hello World')
+
+
+    Num1=int(input("Number 1: "))
+    Num2=int(input("Number 2: "))
+
+    sum=test(Num1,Num2)
+
+    print(sum)
+
+
+
